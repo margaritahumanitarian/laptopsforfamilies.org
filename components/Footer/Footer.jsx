6f69c74451e6cea4391865562265cd196dfe87b1
vincentrohde <vincent.rohde@icloud.com>
2021-10-10 20:39:02 +0200

    add Footer component

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="p-4 grid md:grid-cols-2 bg-neutral text-neutral-content justify-items-center ">
+            <div className="flex items-center justify-evenly">
+                <div>
+                    <Image
+                        alt="mhf logo"
+                        className=""
+                        height="120"
+                        src="/images/MHF-Color-300x300.png"
+                        width="120"
+                    />
+                </div>
+                <div className="pl-4">
+                    {
+                        'Margarita Humanitarian Foundation is a 501(c)(3) non-profit organization that provides humanitarian aid to families in need.'
+                    }
+                </div>
+            </div>
+            <div className="flex items-center">
+                <div>
+                    <a
+                        className="mr-4 mt-2"
+                        href="https://vercel.com?utm_source=margaritahumanitarian&utm_campaign=oss"
+                    >
+                        <Image
+                            alt="Powered by Vercel"
+                            height="44"
+                            src="/images/powered-by-vercel.svg"
+                            width="212"
+                        />
+                    </a>
+                </div>
+                <div>
+                    {' '}
+                    <a
+                        href="https://www.guidestar.org/profile/84-4323038"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <Image
+                            alt="GuideStar Seal of Transparency"
+                            height="80"
+                            src="/images/GuideStarSeal.svg"
+                            width="80"
+                        />
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
